a825ceb39b828b0f8352f7633994b368add16965
gyecham <gyecham@naver.com>
2024-07-16 01:10:33 +0900

    Initial commit

---
+++ Assets/Scripts/ScrollManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ScrollManager : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+
+    void Start()
+    {
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

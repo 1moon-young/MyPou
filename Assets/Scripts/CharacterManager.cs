527cb3e37b7547ede19a39cae0e44bc6092e606d
YUN <85862479+gyecham@users.noreply.github.com>
2024-07-21 00:53:59 +0900

    6일차
    애니메이션 좀 깔짝

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -26,13 +26,15 @@ public class CharacterManager : MonoBehaviour
             Vector3 eyesTargetPosition = new Vector3(mousePosition.x, mousePosition.y, eyesInitialPosition.z);
 
             // 타겟 위치 제한
-            eyesTargetPosition.x = Mathf.Clamp(eyesTargetPosition.x, eyesInitialPosition.x - eyesMaxX*2.5f, eyesInitialPosition.x + eyesMaxX);
-            eyesTargetPosition.y = Mathf.Clamp(eyesTargetPosition.y, eyesInitialPosition.y - eyesMaxY, eyesInitialPosition.y + eyesMaxY-0.02f);
+            eyesTargetPosition.x = Mathf.Clamp(eyesTargetPosition.x, eyesInitialPosition.x - eyesMaxX * 2.5f, eyesInitialPosition.x + eyesMaxX);
+            eyesTargetPosition.y = Mathf.Clamp(eyesTargetPosition.y, eyesInitialPosition.y - eyesMaxY, eyesInitialPosition.y + eyesMaxY - 0.02f);
 
             eyes.localPosition = Vector2.Lerp(eyes.localPosition, eyesTargetPosition, 0.05f);
 
 
-        }else{
+        }
+        else
+        {
             eyes.localPosition = Vector2.Lerp(eyes.localPosition, eyesInitialPosition, 0.05f);
         }
 
